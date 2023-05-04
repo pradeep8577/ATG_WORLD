a4b000e218c6192069abc5e77d9962fe8d5f8986
Pradeep Pawar <72540482+pradeep8577@users.noreply.github.com>
2023-05-05 00:21:20 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,16 @@
+
+const hideSignUp = () =>{
+
+    document.getElementById("create-acc").style.display = "none";
+
+    document.getElementById("sign-in-acc").style.display = "flex" ;
+}
+
+
+
+const hideSignIn = () =>{
+
+    document.getElementById("create-acc").style.display = "flex";
+
+    document.getElementById("sign-in-acc").style.display = "none" ;
+}
